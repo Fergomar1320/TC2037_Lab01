fea26384067c06ee13583ef5f3bcbd5e5fced492
fergomar1320 <09148@portalcedi.edu.mx>
2023-03-10 16:55:24 -0600

    Reflexive and Symetric properties analyzed correctly; Transitive pending

--- Lab_01.py
+++ Lab_01.py
@@ -7,12 +7,15 @@ key_stack = deque()
 alphabet_stack = deque()
 node_stack = deque()
 set = []
-    
+grid = []
+   
 
 def main ():
+    reflexive = True
+    symetric = True
+    transitive = True
     print('Enter your string: ')
     set = input()
-    #print('You entered: ' + set)
 
     #Reading, storing and validating the input syntax
     if set[0] != '{':
@@ -43,8 +46,9 @@ def main ():
                 if element not in alphabet_stack:
                     alphabet_stack.append(element)
 
-    #print(node_stack)
-    #print(alphabet_stack)
+        #Creating a 2D table for the domain of the fucntion
+        grid = [[0 for i in range (len(alphabet_stack))]for j in range(len(alphabet_stack))]
+
 
     #Creating the nodes 
     for index in alphabet_stack:
@@ -52,10 +56,35 @@ def main ():
 
     #Adding the corresponding edges
     for i in range (0,len(node_stack),2):
-        #print (node_stack[i] + ',' + node_stack[i+1])
+        x = int(node_stack[i])
+        y = int(node_stack[i+1])
         dot.edge(node_stack[i], node_stack[i+1])
+        grid[x][y] = 1
+    
+    print(grid)
+
+    #Check if the set is reflexive
+    for i in range(0,len(alphabet_stack)):
+        if grid[i][i] != 1:
+            reflexive = False
+            break
+
+    #Check if the set is symetric
+    for i in range(0,len(alphabet_stack)):
+        for j in range(0,len(alphabet_stack)):
+            print(i)
+            print(j)
+            print(grid[i][j])
+            print(grid[j][i])
+            print('___')
+            if grid[i][j] == 1 and grid[j][i] != 1 or grid[i][j] != 1 and grid[j][i] == 1:
+                symetric = False
+                break
 
     print(dot.source)
+    print(reflexive)
+    print(symetric)
+    #dot.render("Lab01.pdf", view=True)
 
 main()
 
