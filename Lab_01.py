f6d8662f6b480a49a712c86bd5e6f7e0ef78958d
fergomar1320 <09148@portalcedi.edu.mx>
2023-03-10 12:45:53 -0600

    Input read and key sintax validated

---
+++ Lab_01.py
@@ -0,0 +1,46 @@
+import graphviz
+from collections import deque
+
+dot = graphviz.Digraph(comment = 'Lab 01 Fernando Gomez Martinez')
+key_stack = deque()
+node_stack = deque()
+set = []
+    
+
+def main ():
+    print('Enter your string: ')
+    set = input()
+    print('You entered: ' + set)
+
+    #Reading, storing and validating the input syntax
+    if set[0] != '{':
+        print('Syntax Error')
+    else:
+        key_stack.append(set[0])
+        for element in set:
+            if element == '(':
+                if element not in key_stack:
+                    key_stack.append(element)
+                else:
+                    print('ERROR: Misplaced ' + element)
+
+            elif element == ')':
+                last_key = key_stack.pop()
+                if last_key != '(':
+                    print('ERROR: Misplaced ' + element)
+                    break
+
+            elif element == '}':
+                last_key = key_stack.pop()
+                if last_key != '{':
+                    print('ERROR: Misplaced ' + element)
+                    break
+                
+            elif element != ',' and element != ' ' and element != '{':
+                node_stack.append(element)
+
+    print(node_stack)
+
+main()
+
+#{ (0,0), (0,1), (0,3), (1,0), (1,1), (2,2), (3,0), (3,3) }
